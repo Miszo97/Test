d28239b3caa2e51a1e5055818e85e8371029cb03
Miszo <artur@spek.com>
2017-10-09 19:19:06 +0200

    Changes...

--- myprogram.cpp
+++ myprogram.cpp
@@ -2,13 +2,17 @@
 #include <vector>
 #include <algorithm>
 #include <map>
-
+#include "functions.h"
 
 auto main() -> decltype(1){
 
 std::map<int, int > map;
-map.insert(1,2);
+map.insert(std::make_pair(1,2));
+map[1] = 3;
+sayHello();
 
+for(const auto& it: map)
+	std::cout<<it.first<<" "<<it.second<<"\n";
 
 return 1;
 }
